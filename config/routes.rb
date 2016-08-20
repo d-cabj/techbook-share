08da9fadff299024f5fe7fae1ea6c376092759bd
d_ogura <futbolvalent15@gmail.com>
2016-08-20 20:38:59 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:show, :update]
-  resources :items, only: [:show]
+  resources :items, only: [:show, :index] do
+    collection do
+      get 'search'
+      #collectionはRails7つのアクション以外のアクション名を定義する時に使う
+    end
+  end
   resources :category, only: [:show, :index]
 
 end
