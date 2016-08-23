ee10b36cfea3f92bdac4c6437afa4df2012766e4
d_ogura <futbolvalent15@gmail.com>
2016-08-23 18:42:30 +0900

    @itemをビフォアアクションで定義、簡略化 ストロングパラメーターの編集

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -1,21 +1,24 @@
 class BuyController < ApplicationController
+  before_action :item
 
   def new
-    @item = Item.find(params[:id])
     @purchaseHistory = PurchaseHistory.new
   end
 
   def create
     binding.pry
-    電話番号がはいふんありでも保存できるように修正！！！
-    PurchaseHistory.create(purchase_params)
-    binding.pry
-    # (last_name: purchase_params[:last_name], first_name: purchase_params[:first_name], telnumber: purchase_params[:telnumber], postcode: purchase_params[:postcode], address: purchase_params[:address], item_id: params[:id])
+    @purchase_information = PurchaseHistory.create(purchase_params)
   end
 
   private
-    def purchase_params
-      params.permit(:last_name, :first_name, :email, :telnumber, :postcode, :address).merge(item_id: params[:id])
-      # .merge(user_id: current_user.id, item_id: @item.id)
-    end
+  def purchase_params
+    params.permit(:last_name, :first_name, :email, :telnumber, :postcode, :address, :total).merge(item_id: params[:id])
+    # .merge(user_id: current_user.id)
+  end
+
+  def item
+    @item = Item.find(params[:id])
+    @tax = (@item.price * 0.08.to_d).floor
+    @total = @item.price + @item.postage + @tax
+  end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :item, only: :show
   def index
   end
 
   def show
+  end
+
+  private
+  def item
+    @item = Item.find(params[:id])
+    @tax = (@item.price * 0.08.to_d).floor
+    @total = @item.price + @item.postage + @tax
   end
 end
