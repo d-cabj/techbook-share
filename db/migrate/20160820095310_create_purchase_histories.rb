a65f66bef227ff1895a6417ea8c587237a994226
d_ogura <futbolvalent15@gmail.com>
2016-08-23 12:41:16 +0900

    telnumberカラムの型を変更

--- db/migrate/20160820095310_create_purchase_histories.rb
+++ db/migrate/20160820095310_create_purchase_histories.rb
@@ -3,6 +3,12 @@ class CreatePurchaseHistories < ActiveRecord::Migration
     create_table :purchase_histories do |t|
       t.references :user
       t.references :item
+      t.string :last_name
+      t.string :first_name
+      t.string :email
+      t.string :telnumber
+      t.string :postcode
+      t.string :address
       t.timestamps null: false
     end
   end
