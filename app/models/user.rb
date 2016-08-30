0864d7fc33b0acf485799052760da56eaa3e3774
d_ogura <futbolvalent15@gmail.com>
2016-08-30 15:21:55 +0900

    edit model/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,26 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  validates :nickname, :email, presence: true, uniqueness: true
-
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:nickname]
+  validates :nickname, presence: true, uniqueness: true
   has_many :purchaseHistories
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["username = :value", { :value => username }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  # #emailを不要とする
+  # def email_required?
+  #   false
+  # end
+
+  # def email_changed?
+  #   false
+  # end
+
 end
