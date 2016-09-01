3b5135c1dd00601c40f5f51ca88aa48e1131bdcb
d_ogura <futbolvalent15@gmail.com>
2016-09-01 16:55:29 +0900

    delte pry

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -20,7 +20,7 @@ before_action :configure_account_update_params, only: [:update]
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         redirect_to :back
-        binding.pry
+        # binding.pry
       else
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
