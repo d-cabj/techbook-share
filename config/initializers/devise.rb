8d150002bc86a303bf345ad766ad3d368ad424e6
d_ogura <futbolvalent15@gmail.com>
2016-09-05 13:28:07 +0900

    change http method of sign_out  'get' to 'delete'. add http method 'delete'  to relevant link_to option with that in header template.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -245,7 +245,7 @@ Devise.setup do |config|
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   # :getにすることでログアウトできなかった状況を回避
-  config.sign_out_via = :get
+  config.sign_out_via = :delete
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
