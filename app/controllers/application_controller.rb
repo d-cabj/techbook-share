765da5b87bac55e5774b8b72ea34c7f53a1661c4
d_ogura <futbolvalent15@gmail.com>
2016-09-12 16:00:34 +0900

    define judge_login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def judge_login
+    redirect_to root_path, set_flash_message!(:alert, "ログインしてください") unless user_signed_in?
+  end
+
   # Overwriting the sign_out redirect path method
   # def after_sign_out_path_for(resource_or_scope)
   #   root_path
