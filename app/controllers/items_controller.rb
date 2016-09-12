53c2964e02cb8f502a3fb1636f4a931703f7d112
d_ogura <futbolvalent15@gmail.com>
2016-09-12 16:01:33 +0900

    edit items/show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,11 @@
 class ItemsController < ApplicationController
   before_action :item, only: :show
+  before_action :judge_login
   def index
   end
 
   def show
+    @sameCategoryItems = Item.where(category_id: @item.category_id).order("created_at DESC").page(params[:page]).per(3)
   end
 
   private
