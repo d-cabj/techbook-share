b8ef25b420bce9ae61b52b6ed7822502bb75e2f8
d_ogura <futbolvalent15@gmail.com>
2016-09-12 16:04:39 +0900

    define association with admin_user, item model

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,7 @@ class AdminUser < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :items
+  # ユーザーが購入履歴から購入アイテムの参照を可能にするため、dependent: :destroyはしない
+
 end
