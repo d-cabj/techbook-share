cd04b87dba36c5b65ca49caaff2480ee173b1f85
d_ogura <futbolvalent15@gmail.com>
2016-09-12 16:04:07 +0900

    edit buy/new. available to use checkbox.

--- app/controllers/buy_controller.rb
+++ app/controllers/buy_controller.rb
@@ -1,18 +1,29 @@
 class BuyController < ApplicationController
   before_action :item
+  before_action :judge_login
 
   def new
     @purchaseHistory = PurchaseHistory.new
   end
 
   def create
-    binding.pry
-    @purchase_information = PurchaseHistory.create(purchase_params)
+    if params[:checkRulePolicy] == "0"
+      redirect_to :back, alert: "利用規約、プライバシーポリシーに同意頂けたらボックスにチェックしてください"
+    else
+      purchase_params.each do |purchase_param|
+        binding.pry
+         unless purchase_param[1].present?
+           redirect_to :back, alert: "全項目を入力してください"
+           break
+         end
+      end
+      @purchase_information = PurchaseHistory.create(purchase_params)
+    end
   end
 
   private
   def purchase_params
-    params.permit(:last_name, :first_name, :email, :telnumber, :postcode, :address, :total).merge(item_id: params[:id])
+    params.permit(:last_name, :first_name, :email, :telnumber, :postcode, :address, :total).merge(user_id: current_user.id, item_id: params[:id])
     # .merge(user_id: current_user.id)
   end
 
--- app/models/purchase_history.rb
+++ app/models/purchase_history.rb
@@ -1,4 +1,14 @@
 class PurchaseHistory < ActiveRecord::Base
   belongs_to :item
   belongs_to :user
+  attr_accessor :checkRulePolicy
+  validates :postcode,
+    format: {with: /¥d+/, error: "半角数字のみ入力できます"},
+    presence: true
+  validates :telnumber,
+    format: {with: /¥d+/, error: "半角数字のみ入力できます"},
+    presence: true
+  validates :last_name, :first_name, :email, :address, presence: true
+  validates :checkRulePolicy, presence: true
+
 end
