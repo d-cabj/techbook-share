525aa14c51c9d041109d55d6e875e3f0dcfa2e59
d_ogura <futbolvalent15@gmail.com>
2016-09-13 21:06:19 +0900

    検索結果ページの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,21 @@ class ItemsController < ApplicationController
     @sameCategoryItems = Item.where(category_id: @item.category_id).order("created_at DESC").page(params[:page]).per(3)
   end
 
+  def search
+    binding.pry
+    if params[:searchCategory].present? && params[:searchKeyword].present?
+      @resultItems = Item.where(category_id: params[:searchCategory].to_i).where('title LIKE(?)', "%#{params[:searchKeyword]}%").page(params[:page]).per(3)
+    elsif params[:searchCategories].present? && params[:searchKeyword].present? == false
+      @resultItems = Item.where(category_id: params[:searchCategory].to_i).page(params[:page]).per(3)
+    elsif params[:searchCategories].present? == false && params[:searchKeyword].present?
+      @resultItems = Item.where('title LIKE(?)', "%#{params[:searchKeyword]}%").page(params[:page]).per(3)
+    else
+      @resultItems = Item.all.page(params[:page]).per(3)
+    end
+    @searchCategories = Category.all.map {|t| [t.name, t.id]}
+    # @resultitems_count = @resultItems.count →kaminariのAPIで代用可能
+  end
+
   private
   def item
     @item = Item.find(params[:id])
