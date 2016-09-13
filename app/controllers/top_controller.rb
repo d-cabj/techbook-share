44e690a628474a72ac3e7c80b3d8df43db9a0bfd
d_ogura <futbolvalent15@gmail.com>
2016-09-13 21:03:55 +0900

    トップページの検索機能を追加

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
   def index
     @items = Item.all
+    @categories = Category.all
+    @searchCategories = Category.all.map {|t| [t.name, t.id]}
   end
 end
